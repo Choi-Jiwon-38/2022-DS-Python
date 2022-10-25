938bcd74d7d3417d753cba59d448bcf6b50fb662
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-10-25 15:47:13 +0900

    [0919] 페이스북 친구 구하기 코드 - 주석 추가
    - 우리가 원하는 tuple의 형태로 자료를 변환하는 코드에 대한 주석 추가
    - Counter를 이용하여 최빈값을 구하는 과정에 대한 코드에 주석 추가

--- 0919/facebook_friends.py
+++ 0919/facebook_friends.py
@@ -3,10 +3,13 @@ f = open("0919/facebook_combined.txt", "r")
 # 친구수 구하기
 edges = []
 
-for line in f:
+for line in f: # 예시 "1 2"
+    # line.split()을 하면 공백을 기준으로 나누어짐 | "1"과 "2" 따로 분리됨
+    # split()을 통해 나누어진 x를 int로 변환해준 뒤에 튜플의 형태로 변환 | (1, 2)와 같은 tuple의 형태
+    # 튜플 형태로 리스트에 추가
     edges.append(tuple(int(x) for x in line.split()))
 
-num_friends = [0] * 4039
+num_friends = [0] * 4039 # 사람의 수가 4039명인 것을 사전에 알고 있음
 
 for u, v in edges:
     num_friends[u] += 1
@@ -33,6 +36,10 @@ print(f"max: {maxval}")
 from collections import Counter
 cnt = Counter(num_friends)
 print(f"mod: {cnt.most_common(1)[0][0]}")
+# .most_common()을 이용하여 가장 많은 것부터 차례대로 나타남 | 형태는 [(3, 6), (2, 3), (1, 1), (4, 1)]과 같은 형태
+# .most_common(1)을 이용하면 가장 많은 것 하나만 나타남 | 형태는 [(3, 6)]
+# [0]을 이용하여 접근 -> (3, 6)
+# 한 번 더 [0]을 이용하여 가장 많이 나온 수를 구하거나 [1]을 이용하여 그 횟수를 구할 수 있음
 
 # quantile 분위
 q25 = num_friends_sorted[int(len(num_friends_sorted) * 0.25)]
